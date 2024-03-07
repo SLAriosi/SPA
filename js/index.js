70819a6da63cbf8e66e48fa6cce5a5c5bb57d798
SLAriosi <ariosilucas@gmail.com>
2024-03-07 11:51:49 -0300

    Mais conceitos de Orientação a Objeto

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-import "./router.js"
-const routes = {
-   "/": "/pages/home.html",
-   "/about": "/pages/about.html",
-   "/contact": "/pages/contact.html",
-   404: "/pages/404.html",
-}
+import { Router } from "./router.js"
 
-window.onpopstate = () => handle()
-window.route = () => route()
+const router = new Router()
+
+router.add("/","/pages/home.html")
+router.add("/about","/pages/about.html")
+router.add("/contact","/pages/contact.html")
+router.add(404,"/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
